ba815c5d2c1c193733f4921349a0239bf00fbd05
Nikita Biryukov <nikitabirukov@gmail.com>
2024-05-29 15:47:23 +0300

    add rnn

---
+++ src/models/rnn.py
@@ -0,0 +1,14 @@
+from torch import nn
+
+ 
+class CharModel(nn.Module):
+    def __init__(self, n_vocab):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=1, hidden_size=256, num_layers=1, batch_first=True)
+        self.dropout = nn.Dropout(0.2)
+        self.linear = nn.Linear(256, n_vocab)
+    def forward(self, x):
+        x, _ = self.lstm(x)
+        x = x[:, -1, :]
+        x = self.linear(self.dropout(x))
+        return x
